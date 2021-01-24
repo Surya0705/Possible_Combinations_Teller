36e701201e505528e7c035881db281b89a5da7e2
Surya Thakur <77577564+Surya0705@users.noreply.github.com>
2021-01-24 18:19:35 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,4 @@
+from itertools import permutations # Importing the required Modules
+
+a = list(permutations(range(1, 4))) # Giving it a range. You may replace 1 and 4 with numbers of Your Choice.
+print(a) # Printing the Results.
